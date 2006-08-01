e32ec1737bea0b4723a71801af41c6eea3ece295
cvs2git <feedback@pld-linux.org>
2006-08-01 13:11:59 +0000

    This commit was manufactured by cvs2git to create tag 'auto-th-mozilla- firefox-2_0-0_2'.
    Sprout from master 2006-08-01 13:11:59 UTC havner <havner@pld-linux.org> '- unify it to 16/14, those settings are in pixels not points (ARGH!!!)'
    Cherrypick from master 2005-03-03 15:01:46 UTC Arkadiusz Miśkiewicz <arekm@maven.pl> '- drop "':
        mozilla-firefox.sh -> 1.6
    Cherrypick from DEVEL 2005-09-08 23:20:49 UTC Arkadiusz Miśkiewicz <arekm@maven.pl> '- deep park':
        mozilla-firefox-nss-system-nspr.patch -> 1.1.2.1

--- mozilla-firefox.sh
+++ mozilla-firefox.sh
@@ -1,45 +1,18 @@
 #!/bin/sh
 # based on script by (c) vip at linux.pl, wolf at pld-linux.org
 
-LIBDIR="@LIBDIR@/mozilla-firefox"
+if [ `arch` == "x86_64" ]; then
+	LIBDIR="/usr/lib64/mozilla-firefox"
+else
+	LIBDIR="/usr/lib/mozilla-firefox"
+fi
 
 MOZILLA_FIVE_HOME=$LIBDIR
 
-MOZARGS=
-MOZLOCALE="$(/usr/bin/locale | grep "^LC_MESSAGES=" | \
-		sed -e "s|LC_MESSAGES=||g" -e "s|\"||g" )"
-for MOZLANG in $(echo $LANGUAGE | tr ":" " ") $MOZLOCALE; do
-	eval MOZLANG="$(echo $MOZLANG | sed -e "s|_\([^.]*\).*|-\1|g")"
+MOZLOCALE="$(/usr/bin/locale | grep "^LC_MESSAGES=" | sed -e "s|LC_MESSAGES=||g" -e "s|\"||g" )"
+eval MOZLOCALE="$(echo $MOZLOCALE | sed -e "s|_\([^.]*\).*|-\1|g")"
 
-	if [ -f $MOZILLA_FIVE_HOME/chrome/$MOZLANG.jar ]; then
-		MOZARGS="-UILocale $MOZLANG"
-		break
-	fi
-done
-
-if [ -z "$MOZARGS" ]; then
-	# try harder
-	for MOZLANG in $(echo $LANGUAGE | tr ":" " ") $MOZLOCALE; do
-		eval MOZLANG="$(echo $MOZLANG | sed -e "s|_.*||g")"
-
-		LANGFILE=$(echo ${MOZILLA_FIVE_HOME}/chrome/${MOZLANG}*.jar \
-				| sed 's/\s.*//g' )
-		if [ -f "$LANGFILE" ]; then
-			MOZLANG=$(basename "$LANGFILE" | sed 's/\.jar//')
-			MOZARGS="-UILocale $MOZLANG"
-			break
-		fi
-	done
-fi
-
-# compreg.dat and/or chrome.rdf will screw things up if it's from an
-# older version.  http://bugs.gentoo.org/show_bug.cgi?id=63999
-for f in ~/{.,.mozilla/}firefox/*/{compreg.dat,chrome.rdf,XUL.mfasl}; do
-	if [[ -f ${f} && ${f} -ot /usr/bin/mozilla-firefox ]]; then
-		echo "Removing ${f} leftover from older firefox"
-		rm -f "${f}"
-	fi
-done
+[ -f $MOZILLA_FIVE_HOME/chrome/$MOZLOCALE.jar ] && MOZARGS="-UILocale $MOZLOCALE"
 
 if [ -n "$MOZARGS" ]; then
 	FIREFOX="$LIBDIR/firefox $MOZARGS"
@@ -48,22 +21,22 @@ else
 fi
 
 if [ "$1" == "-remote" ]; then
-	exec $FIREFOX "$@"
+	$FIREFOX "$@"
 else
 	PING=`$FIREFOX -remote 'ping()' 2>&1 >/dev/null`
 	if [ -n "$PING" ]; then
 		if [ -f "`pwd`/$1" ]; then
-			exec $FIREFOX "file://`pwd`/$1"
+			$FIREFOX "file://`pwd`/$1"
 		else
-			exec $FIREFOX "$@"
+			$FIREFOX "$@"
 		fi
 	else
 		if [ -z "$1" ]; then
-			exec $FIREFOX -remote 'xfeDoCommand(openBrowser)'
+			$FIREFOX -remote 'xfeDoCommand (openBrowser)'
 		elif [ "$1" == "-mail" ]; then
-			exec $FIREFOX -remote 'xfeDoCommand(openInbox)'
+			$FIREFOX -remote 'xfeDoCommand (openInbox)'
 		elif [ "$1" == "-compose" ]; then
-			exec $FIREFOX -remote 'xfeDoCommand(composeMessage)'
+			$FIREFOX -remote 'xfeDoCommand (composeMessage)'
 		else
 			if [ -f "`pwd`/$1" ]; then
 				URL="file://`pwd`/$1"
@@ -72,9 +45,9 @@ else
 			fi
 			grep browser.tabs.opentabfor.middleclick ~/.mozilla/firefox/*/prefs.js | grep true > /dev/null
 			if [ 0 -eq 0 ]; then
-				exec $FIREFOX -new-tab "$URL"
+				$FIREFOX -remote "OpenUrl($URL,new-tab)"
 			else
-				exec $FIREFOX -new-window "$URL"
+				$FIREFOX -remote "OpenUrl($URL,new-window)"
 			fi
 		fi
 	fi
