beab7ea6170e3241c013e1d8019e22d8086e61c2
hawk <hawk@pld-linux.org>
2008-06-23 10:15:44 +0000

    - taken from firefox 3.0 for new iceweasel
    Changed files:
        iceweasel.desktop -> 1.3
        iceweasel.sh -> 1.2

--- iceweasel.sh
+++ iceweasel.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 # based on script by (c) vip at linux.pl, wolf at pld-linux.org
-# using mozilla-launcher by glen at pld-linux.org
-# copied from swiftfox.sh by czarny at pld-linux.org
-#
-# Stub script to run mozilla-launcher.  We used to use a symlink here
-# but OOo brokenness makes it necessary to use a stub instead:
-# http://bugs.gentoo.org/show_bug.cgi?id=78890
 
-export MOZILLA_LAUNCHER=@APPNAME@
-export MOZILLA_LIBDIR=@LIBDIR@/@APPNAME@
+LIBDIR="@LIBDIR@/iceweasel"
 
-mozlocale=$(/usr/bin/locale | awk -F= '/^LC_MESSAGES=/{print $2}' | xargs)
-mozlocale="$LANGUAGE ${mozlocale%.*}"
-for MOZLANG in $mozlocale; do
-	MOZLANG=$(echo $MOZLANG | sed -e 's|_\([^.]*\).*|-\1|')
+MOZARGS=
+MOZLOCALE="$(/usr/bin/locale | grep "^LC_MESSAGES=" | \
+		sed -e "s|LC_MESSAGES=||g" -e "s|\"||g" )"
+for MOZLANG in $(echo $LANGUAGE | tr ":" " ") $MOZLOCALE; do
+	eval MOZLANG="$(echo $MOZLANG | sed -e "s|_\([^.]*\).*|-\1|g")"
 
-	if [ -f $MOZILLA_LIBDIR/chrome/$MOZLANG.jar ]; then
+	if [ -f $LIBDIR/chrome/$MOZLANG.jar ]; then
 		MOZARGS="-UILocale $MOZLANG"
 		break
 	fi
@@ -23,10 +17,11 @@ done
 
 if [ -z "$MOZARGS" ]; then
 	# try harder
-	for MOZLANG in $mozlocale; do
-		MOZLANG=$(echo $MOZLANG | sed -e 's|_.*||')
+	for MOZLANG in $(echo $LANGUAGE | tr ":" " ") $MOZLOCALE; do
+		eval MOZLANG="$(echo $MOZLANG | sed -e "s|_.*||g")"
 
-		LANGFILE=$(echo ${MOZILLA_LIBDIR}/chrome/${MOZLANG}*.jar | sed 's/\s.*//g')
+		LANGFILE=$(echo $LIBDIR/chrome/${MOZLANG}*.jar \
+				| sed 's/\s.*//g' )
 		if [ -f "$LANGFILE" ]; then
 			MOZLANG=$(basename "$LANGFILE" | sed 's/\.jar//')
 			MOZARGS="-UILocale $MOZLANG"
@@ -35,4 +30,50 @@ if [ -z "$MOZARGS" ]; then
 	done
 fi
 
-exec /usr/lib/mozilla-launcher $MOZARGS "$@"
+# compreg.dat and/or chrome.rdf will screw things up if it's from an
+# older version.  http://bugs.gentoo.org/show_bug.cgi?id=63999
+for f in ~/{.,.mozilla/}iceweasel/*/{compreg.dat,chrome.rdf,XUL.mfasl}; do
+	if [[ -f ${f} && ${f} -ot /usr/bin/iceweasel ]]; then
+		echo "Removing ${f} leftover from older iceweasel"
+		rm -f "${f}"
+	fi
+done
+
+if [ -n "$MOZARGS" ]; then
+	ICEWEASEL="$LIBDIR/iceweasel $MOZARGS"
+else
+	ICEWEASEL="$LIBDIR/iceweasel"
+fi
+
+if [ "$1" == "-remote" ]; then
+	exec $ICEWEASEL "$@"
+else
+	PING=`$ICEWEASEL -remote 'ping()' 2>&1 >/dev/null`
+	if [ -n "$PING" ]; then
+		if [ -f "`pwd`/$1" ]; then
+			exec $ICEWEASEL "file://`pwd`/$1"
+		else
+			exec $ICEWEASEL "$@"
+		fi
+	else
+		if [ -z "$1" ]; then
+			exec $ICEWEASEL -remote 'xfeDoCommand(openBrowser)'
+		elif [ "$1" == "-mail" ]; then
+			exec $ICEWEASEL -remote 'xfeDoCommand(openInbox)'
+		elif [ "$1" == "-compose" ]; then
+			exec $ICEWEASEL -remote 'xfeDoCommand(composeMessage)'
+		else
+			if [ -f "`pwd`/$1" ]; then
+				URL="file://`pwd`/$1"
+			else
+				URL="$1"
+			fi
+			grep browser.tabs.opentabfor.middleclick ~/.mozilla/iceweasel/*/prefs.js | grep false > /dev/null
+			if [ $? -ne 0 ]; then
+				exec $ICEWEASEL -new-tab "$URL"
+			else
+				exec $ICEWEASEL -new-window "$URL"
+			fi
+		fi
+	fi
+fi
