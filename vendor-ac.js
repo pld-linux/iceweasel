13fd9565dd58246f74f2f9ad139d55d23f49a42a
Jan Rękorajski <baggins@pld-linux.org>
2012-08-19 13:50:03 +0200

    Replace patch with vendor-specific config

---
+++ vendor-ac.js
@@ -0,0 +1,8 @@
+pref("general.useragent.vendor", "PLD");
+pref("general.useragent.vendorSub", "2.0");
+pref("general.useragent.vendorComment", "Ac");
+
+pref("general.useragent.compatMode.firefox", true);
+pref("distribution.searchplugins.defaultLocale", "en-US");
+// Forbid application updates
+lockPref("app.update.enabled", false);
